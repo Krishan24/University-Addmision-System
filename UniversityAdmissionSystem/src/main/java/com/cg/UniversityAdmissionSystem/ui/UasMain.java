eedda1990cfafa8a945c7ac24f791a6247fce22b
hp <hp@DESKTOP-NJ8LB0P>
2020-04-04 03:57:08 -0700

    commit by charu in main

---
+++ UniversityAdmissionSystem/src/main/java/com/cg/UniversityAdmissionSystem/ui/UasMain.java
@@ -0,0 +1,10 @@
+package com.cg.UniversityAdmissionSystem.ui;
+
+public class UasMain {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("commit from charul");
+	}
+
+}
