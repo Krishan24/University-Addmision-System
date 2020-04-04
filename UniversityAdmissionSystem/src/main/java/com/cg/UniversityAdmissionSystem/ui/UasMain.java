0310ff6c0e30619ee409ff649e35eb837bac985c
Lenovo <Lenovo@satya-pavani>
2020-04-04 16:01:02 +0530

    test commit

--- UniversityAdmissionSystem/src/main/java/com/cg/UniversityAdmissionSystem/ui/UasMain.java
+++ UniversityAdmissionSystem/src/main/java/com/cg/UniversityAdmissionSystem/ui/UasMain.java
@@ -5,6 +5,7 @@ public class UasMain {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("test git commit");
+		System.out.println("commit for test");
 	}
 
 }
