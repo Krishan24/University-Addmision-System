b18dd41062f34171f3a0053f7cfd901a3e79315e
Lenovo <Lenovo@satya-pavani>
2020-04-05 13:08:39 +0530

    completed the validation part

---
+++ UniversityAdmissionSystem/src/main/java/com/cg/UnivesityAdmissionSystem/utility/Validation.java
@@ -0,0 +1,94 @@
+package com.cg.UnivesityAdmissionSystem.utility;
+
+public class Validation {
+
+	public static boolean firstNameValidation(String firstName)
+	{
+		String pattern = "[A-Za-z]{4,20}";
+		if(firstName.matches(pattern))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	public static boolean lastNameValidation(String lastName)
+	{
+		String pattern = "[A-Za-z]{4,20}";
+		if(lastName.matches(pattern))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	public static boolean dateOfBirthValidation(String dateOfBirth)
+	{
+		String pattern = "[0-9]{2}[/]{1}[0-9]{2}[/]{1}[0-9]{4";
+		if(dateOfBirth.matches(pattern))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	public static boolean fatherNameValidation(String fatherName)
+	{
+		String pattern = "[A-Za-z]{4,30}";
+		if(fatherName.matches(pattern))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	public static boolean emailValidation(String email)
+	{
+		String pattern = "[A-Za-z]{4,20}[@]{1}[gmail]{5}[.com]{4}";
+		if(email.matches(pattern))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	public static boolean mobileNumberValidation(String mobileNumber)
+	{
+		String pattern = "[6-9]{1}[0-9]{9}";
+		if(mobileNumber.matches(pattern))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	public static boolean userNameValidation(String userName)
+	{
+		String pattern = "[A-Za-z]{4,20}[@]{1}[gmail]{5}[.com]{4}";
+		if(userName.matches(pattern))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	public static boolean passwordValidation(String password)
+	{
+		String pattern = "[A-Za-z]{3,6}[0-9]{2}[@#$%&*]{1}";
+		if(password.matches(pattern))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	public static boolean optionValidate(String option) {
+		String pattern = "[1-4]{1}";
+		if(option.matches(pattern))
+			return true;
+		else
+		return false;
+	}
+	
+	
+}
