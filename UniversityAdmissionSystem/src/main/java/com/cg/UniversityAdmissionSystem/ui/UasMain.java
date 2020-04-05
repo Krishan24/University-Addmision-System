3d5ae8ea37e448d943a30fa467a7f2f8dbb2ba31
hp <hp@DESKTOP-NJ8LB0P>
2020-04-05 05:15:17 -0700

    commit done here

--- UniversityAdmissionSystem/src/main/java/com/cg/UniversityAdmissionSystem/ui/UasMain.java
+++ UniversityAdmissionSystem/src/main/java/com/cg/UniversityAdmissionSystem/ui/UasMain.java
@@ -7,7 +7,7 @@ public class UasMain {
 	public static void main(String[] args) {
 		
 		Scanner scr = new Scanner(System.in);
-		System.out.println("Welcome to University Admission System");
+		System.out.println("-----------Welcome to University Admission System----------");
 //		System.out.println("Following Courses are as follows:1.B.Tech 2.BBA 3.BCA 4.MBA ");
 		System.out.println("Please Chhose the Offered Programs:- \n"+"1.B.Tech \n" + "2.BBA \n" +"3.BCA \n" + "4.MBA \n" +"5.Exit");
 		int option = scr.nextInt();
@@ -26,11 +26,12 @@ public class UasMain {
 		       System.out.println("Please enter the Email");
 		       String email = scr.next();
 		       System.out.println("Please enter the Mobile Number");
-		       String mobnum = scr.next();
+		       long mobnum = scr.nextLong();
 		       System.out.println("Please enter the SSC Marks");
-		       String ssc = scr.next();
+		       double ssc = scr.nextDouble();
 		       System.out.println("Please enter the HSC Marks");
-		       String hsc = scr.next();
+		       double hsc = scr.nextDouble();
+		       System.out.println("Successfully Submit the Form");
 		       break;
 		       
 		case 2: System.out.println("Online Application form for BBA");
@@ -45,11 +46,12 @@ public class UasMain {
 	       		System.out.println("Please enter the Email");
 	       		String email1 = scr.next();
 	       		System.out.println("Please enter the Mobile Number");
-	       		String mobnum1 = scr.next();
+	       		long mobnum1 = scr.nextLong();
 	       		System.out.println("Please enter the SSC Marks");
-	       		String ssc1= scr.next();
+	       		double ssc1= scr.nextDouble();
 	       		System.out.println("Please enter the HSC Marks");
-	       		String hsc1 = scr.next();
+	       		double hsc1 = scr.nextDouble();
+	       		System.out.println("Successfully Submit the Form");
 	       		break;
 	       		
 		case 3: System.out.println("Online Application form for BCA");
@@ -64,11 +66,12 @@ public class UasMain {
 		   		System.out.println("Please enter the Email");
 		   		String email2 = scr.next();
 		   		System.out.println("Please enter the Mobile Number");
-		   		String mobnum2 = scr.next();
+		   		long mobnum2 = scr.nextLong();
 		   		System.out.println("Please enter the SSC Marks");
-		   		String ssc2= scr.next();
+		   		double ssc2= scr.nextDouble();
 		   		System.out.println("Please enter the HSC Marks");
-		   		String hsc2 = scr.next();
+		   		double hsc2 = scr.nextDouble();
+		   		System.out.println("Successfully Submit the Form");
 		   		break;
 		   		
 		case 4: System.out.println("Online Application form for MBA");
@@ -83,16 +86,20 @@ public class UasMain {
 		   		System.out.println("Please enter the Email");
 		   		String email3 = scr.next();
 		   		System.out.println("Please enter the Mobile Number");
-		   		String mobnum3 = scr.next();
+		   		long mobnum3 = scr.nextLong();
 		   		System.out.println("Please enter the SSC Marks");
-		   		String ssc3= scr.next();
+		   		double ssc3= scr.nextDouble();
 		   		System.out.println("Please enter the HSC Marks");
-		   		String hsc3 = scr.next();
+		   		double hsc3 = scr.nextDouble();
+		   		System.out.println("Successfully Submit the Form");
 		   		break; 
 		   		
 		   		
-		case 5: 
+		case 5: System.out.println("Thank You For Visiting");
 			System.exit(0);
+			
+			
+			
 	           
 		       
 		}
