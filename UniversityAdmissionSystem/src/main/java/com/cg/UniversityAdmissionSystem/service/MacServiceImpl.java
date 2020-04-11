d613101ac3453e7f02c2e8e0c4c3d0d7faadd55f
Lenovo <Lenovo@satya-pavani>
2020-04-11 16:39:23 +0530

    implemented admission committee

--- UniversityAdmissionSystem/src/main/java/com/cg/UniversityAdmissionSystem/service/MacServiceImpl.java
+++ UniversityAdmissionSystem/src/main/java/com/cg/UniversityAdmissionSystem/service/MacServiceImpl.java
@@ -1,24 +1,51 @@
 package com.cg.UniversityAdmissionSystem.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import com.cg.UniversityAdmissionSystem.bean.ApplicationBean;
 
 public class MacServiceImpl implements IMacService {
 
-	public List<String> viewAllAplicationsOfSpecPro(String program) {
+   public List<Integer> getApplicantId() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public boolean updateApplication(ApplicationBean stu) {
+	public List<String> getScheduleId() {
 		// TODO Auto-generated method stub
-		return false;
+		return null;
 	}
 
-	public void updateApplicationStatus() {
+	public List<ApplicationBean> getApplicantOnScheduledId(String scheduledProgramId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<ApplicationBean> getApplicantAfterInterviewOnId(String scheduledProgramId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateApplicantStatus(int applicationId, String status) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateApplicantDateOfInterview(int applicationId, LocalDate dateOfInterview) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public static boolean validateScheduledId(String id)
+	{
+		String pattern = "[0-9]{4}";
+		if(id.matches(pattern))
+		{
+			return true;
+		}
+		else
+		 return false;
 	}
 
 }
